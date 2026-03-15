8138c7eaf776829477008c02bb89b62fa68874be
Binh Phuc <pham.phuc.binh.271106@gmail.com>
2026-03-15 20:36:05 +0700

    fix()

--- src/components/PdfReader.jsx
+++ src/components/PdfReader.jsx
@@ -3,10 +3,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export default function PdfReader({ book, onClose }) {
   const [numPages, setNumPages] = useState(null);
